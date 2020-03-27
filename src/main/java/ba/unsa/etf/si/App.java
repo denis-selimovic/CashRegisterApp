38cfd72884a9459a94f4538de10fc038d2d2052f
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-03-28 00:01:25 +0100

    Dependency injection in controller

--- src/main/java/ba/unsa/etf/si/App.java
+++ src/main/java/ba/unsa/etf/si/App.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.si;
 
+import ba.unsa.etf.si.controllers.PrimaryController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
@@ -19,12 +20,11 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("fxml/primary.fxml"), 640, 480);
-        Rectangle2D rect = getScreenSize();
-        stage.setWidth(rect.getWidth());
-        stage.setHeight(rect.getHeight());
-        stage.setScene(scene);
-        stage.setMaximized(true);
+        //scene = new Scene(loadFXML("fxml/primary.fxml"), 640, 480);
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("fxml/primary.fxml"));
+        loader.setController(new PrimaryController(PrimaryController.Role.CHIEF));
+        scene = new Scene(loader.load());
+        setStage(stage);
         stage.show();
     }
 
@@ -36,6 +36,15 @@ public class App extends Application {
     public static Rectangle2D getScreenSize() {
         Screen screen = Screen.getPrimary();
         return screen.getBounds();
+    }
+
+    private static void setStage(Stage stage) {
+        Rectangle2D rect = getScreenSize();
+        stage.setWidth(rect.getWidth());
+        stage.setHeight(rect.getHeight());
+        stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.setResizable(false);
     }
 
     public static void main(String[] args) {

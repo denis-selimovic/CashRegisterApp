6d6031a1c27594c08ba2c9e05cb3a6195d9ee39e
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-03-28 14:02:45 +0100

    Added css classes

--- src/main/java/ba/unsa/etf/si/controllers/ReceiptController.java
+++ src/main/java/ba/unsa/etf/si/controllers/ReceiptController.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.si.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 public class ReceiptController {
+
+
+    @FXML private ListView<String> receiptList;
+
+    @FXML
+    public void initialize() {
+        receiptList.getItems().addAll("Denis", "Denis2", "Denis3");
+    }
 }

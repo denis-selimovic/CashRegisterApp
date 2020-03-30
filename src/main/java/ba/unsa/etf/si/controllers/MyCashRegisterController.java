2806d6b799f2a780bc8a5f0e298b18ec750fa90f
dselimovic1 <44275362+dselimovic1@users.noreply.github.com>
2020-03-30 17:59:59 +0200

    Update MyCashRegisterController.java

--- src/main/java/ba/unsa/etf/si/controllers/MyCashRegisterController.java
+++ src/main/java/ba/unsa/etf/si/controllers/MyCashRegisterController.java
@@ -3,16 +3,21 @@ package ba.unsa.etf.si.controllers;
 import ba.unsa.etf.si.App;
 import ba.unsa.etf.si.models.Product;
 import ba.unsa.etf.si.utility.HttpUtils;
+import com.jfoenix.controls.JFXButton;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.util.Callback;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.net.http.HttpRequest;
@@ -30,18 +35,12 @@ public class MyCashRegisterController {
     public TableColumn<Product, String> total;
     public TableView<Product> receiptTable;
 
-    public Label productListLabel;
-    public TableView<Product> productsTable;
-    public TableColumn<Product, Integer> productId;
-    public TableColumn<Product, String> productTitle;
 
+    @FXML private ListView<Product> productsTable;
 
-    @FXML
-    private ChoiceBox<String> myCashRegisterSearchFilters;
-    @FXML
-    private TextField myCashRegisterSearchInput;
-    @FXML
-    private Label price;
+    @FXML private ChoiceBox<String> myCashRegisterSearchFilters;
+    @FXML private TextField myCashRegisterSearchInput;
+    @FXML private Label price;
 
     private ObservableList<Product> products = FXCollections.observableArrayList();
 
@@ -75,21 +74,20 @@ public class MyCashRegisterController {
         });
         addRemoveButtonToTable();
 
-        productId.setCellValueFactory(new PropertyValueFactory<>("id"));
-        productTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
-        addButtonToTable();
+
+        productsTable.setCellFactory(new ProductCellFactory());
         getProducts();
         myCashRegisterSearchInput.textProperty().addListener((observableValue, oldValue, newValue) -> {
-            if (newValue == null || newValue.isEmpty()) {
+            if(newValue == null || newValue.isEmpty()) {
                 productsTable.setItems(products);
                 return;
             }
-            if (!oldValue.equals(newValue)) search();
+            if(!oldValue.equals(newValue)) search();
         });
     }
 
     public double price() {
-        return receiptTable.getItems().stream().mapToDouble(p -> {
+        return receiptTable.getItems().stream().mapToDouble( p -> {
             String format = String.format("%.2f", p.getTotalPrice());
             return Double.parseDouble(format);
         }).sum();
@@ -122,14 +120,15 @@ public class MyCashRegisterController {
         int id;
         try {
             id = Integer.parseInt(text);
-        } catch (NumberFormatException e) {
+        }
+        catch (NumberFormatException e) {
             id = -1;
         }
         return id;
     }
 
     private ObservableList<Product> filterByID(int id) {
-        if (id == -1) return FXCollections.observableArrayList();
+        if(id == -1) return FXCollections.observableArrayList();
         return products.stream().filter(p -> p.getId() == id).collect(Collectors.collectingAndThen(Collectors.toList(), FXCollections::observableArrayList));
     }
 
@@ -142,7 +141,9 @@ public class MyCashRegisterController {
         HttpUtils.send(GET, HttpResponse.BodyHandlers.ofString(), response -> {
             try {
                 products = Product.getProductListFromJSON(response);
-            } catch (Exception e) {
+                products.add(new Product(1, "Sok", 27.5, 13.0, 5.0));
+            }
+            catch (Exception e) {
                 e.printStackTrace();
             }
             Platform.runLater(() -> {
@@ -161,10 +162,12 @@ public class MyCashRegisterController {
             public TableCell<Product, Void> call(final TableColumn<Product, Void> param) {
                 final TableCell<Product, Void> cell = new TableCell<Product, Void>() {
 
-                    private final Button btn = new Button("Remove");
+                    private final Button btn = new Button();
 
                     {
                         btn.setOnAction(e -> removeFromReceipt(indexProperty().get()));
+                        btn.setGraphic(new ImageView(new Image(App.class.getResourceAsStream("img/cancel.png"))));
+                        btn.getStyleClass().add("btn");
                     }
 
                     @Override
@@ -182,7 +185,7 @@ public class MyCashRegisterController {
         };
 
         colBtn.setCellFactory(cellFactory);
-
+        colBtn.setResizable(false);
         receiptTable.getColumns().add(colBtn);
     }
 
@@ -192,41 +195,6 @@ public class MyCashRegisterController {
         price.setText(showPrice());
     }
 
-    public void addButtonToTable() {
-        TableColumn<Product, Void> buttonColumn = new TableColumn<>("Add");
-        Callback<TableColumn<Product, Void>, TableCell<Product, Void>> cellFactory = new Callback<>() {
-            @Override
-            public TableCell<Product, Void> call(TableColumn<Product, Void> productVoidTableColumn) {
-                final TableCell<Product, Void> cell = new TableCell<Product, Void>() {
-                    private final Button btnAction = new Button("Add to Cart");
-
-                    {
-                        btnAction.setOnAction(event -> {
-                            Product p = getTableView().getItems().get(getIndex());
-                            if (!receiptTable.getItems().contains(p)) {
-                                receiptTable.getItems().add(p);
-                                price.setText(showPrice());
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void updateItem(Void item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (empty) {
-                            setGraphic(null);
-                        } else {
-                            setGraphic(btnAction);
-                        }
-                    }
-
-                };
-                return cell;
-            }
-        };
-        buttonColumn.setCellFactory(cellFactory);
-        productsTable.getColumns().add(buttonColumn);
-    }
 
     class EditingCell extends TableCell<Product, String> {
 
@@ -283,21 +251,22 @@ public class MyCashRegisterController {
                 }
             });
             textField.setOnKeyPressed(e -> {
-                if (e.getCode().equals(KeyCode.ENTER)) {
+                if(e.getCode().equals(KeyCode.ENTER)) {
                     int current = indexProperty().get();
-                    if (getText().isEmpty()) {
+                    if(getText().isEmpty()) {
                         getTableView().getItems().get(current).setTotal(1);
                         setText("1");
                     }
-                    if (getText().equals("0")) {
+                    if(getText().equals("0")) {
                         removeFromReceipt(current);
                         return;
                     }
                     Product p = getTableView().getItems().get(current);
-                    if (p.getQuantity() < Integer.parseInt(getText())) {
+                    if(p.getQuantity() < Integer.parseInt(getText())) {
                         p.setTotal((int) p.getQuantity());
                         setText(Integer.toString(p.getTotal()));
-                    } else p.setTotal(Integer.parseInt(getText()));
+                    }
+                    else p.setTotal(Integer.parseInt(getText()));
                     getTableView().getColumns().get(current).setVisible(false);
                     getTableView().getColumns().get(current).setVisible(true);
                     price.setText(showPrice());
@@ -309,4 +278,54 @@ public class MyCashRegisterController {
             return getItem() == null ? "" : getItem();
         }
     }
+
+    public final class ProductCell extends ListCell<Product> {
+
+        @FXML private Label productID, name;
+        @FXML private JFXButton addBtn;
+
+        public ProductCell() {
+            loadFXML();
+        }
+
+        private void loadFXML() {
+            FXMLLoader loader = new FXMLLoader(App.class.getResource("fxml/product.fxml"));
+            loader.setController(this);
+            loader.setRoot(this);
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected void updateItem(Product product, boolean empty) {
+            super.updateItem(product, empty);
+            if(empty) {
+                setText(null);
+                setContentDisplay(ContentDisplay.TEXT_ONLY);
+            }
+            else {
+                productID.setText(Long.toString(product.getId()));
+                name.setText(product.getTitle());
+                addBtn.setOnAction(e -> {
+                    if(!receiptTable.getItems().contains(product)) {
+                        receiptTable.getItems().add(product);
+                        price.setText(showPrice());
+                    }
+                });
+                setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+            }
+        }
+    }
+
+    public class ProductCellFactory implements Callback<ListView<Product>, ListCell<Product>> {
+
+        @Override
+        public ListCell<Product> call(ListView<Product> param) {
+            return new ProductCell();
+        }
+    }
+
 }

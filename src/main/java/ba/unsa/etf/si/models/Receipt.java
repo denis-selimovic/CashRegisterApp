5bdb96b43d64fb6976c777cf843a4daf6cb2b884
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-04-02 15:27:57 +0200

    Added model

--- src/main/java/ba/unsa/etf/si/models/Receipt.java
+++ src/main/java/ba/unsa/etf/si/models/Receipt.java
@@ -1,13 +1,43 @@
 package ba.unsa.etf.si.models;
 
-import java.time.LocalDateTime;
+import ba.unsa.etf.si.models.status.PaymentMethod;
+import ba.unsa.etf.si.models.status.ReceiptStatus;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "receipts")
 public class Receipt {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Transient
+    private String receiptID;
+
+    @Transient
+    private PaymentMethod paymentMethod;
+
+    @Transient
+    private ReceiptStatus receiptStatus;
+
+    @Basic
+    @Temporal(value = TemporalType.TIMESTAMP)
     private LocalDateTime date;
+
     private String cashier;
     private Double amount;
+
+    @ManyToOne
+    @JoinColumn(name = "receipt_id")
+    private List<Receipt> receiptItems;
+
+    public Receipt() {
+
+    }
 
     public Receipt(Long id, LocalDateTime date, String cashier, Double amount) {
         this.id = id;
@@ -44,7 +74,32 @@ public class Receipt {
         return amount;
     }
 
-    public void setAmount(Double amount) {
-        this.amount = amount;
+
+    public String getReceiptID() {
+        return receiptID;
+    }
+
+    public ReceiptStatus getReceiptStatus() {
+        return receiptStatus;
+    }
+
+    public void setReceiptStatus(ReceiptStatus receiptStatus) {
+        this.receiptStatus = receiptStatus;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public List<Receipt> getReceiptItems() {
+        return receiptItems;
+    }
+
+    public void setReceiptItems(List<Receipt> receiptItems) {
+        this.receiptItems = receiptItems;
     }
 }
---
+++ src/main/java/ba/unsa/etf/si/models/status/ReceiptItem.java
@@ -0,0 +1,49 @@
+package ba.unsa.etf.si.models.status;
+
+import ba.unsa.etf.si.models.Product;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "receipt_items")
+public class ReceiptItem {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private double quantity;
+
+    public ReceiptItem() {
+        product = null;
+        quantity = 0;
+    }
+
+    public ReceiptItem(Product product, double quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}

d86f81de16d9af1797a471f5b4af9ddbbbd5089c
Lino2007 <44145624+Lino2007@users.noreply.github.com>
2020-04-03 20:13:42 +0200

    Dizajniran dijalog box

---
+++ src/main/java/ba/unsa/etf/si/controllers/DialogController.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.si.controllers;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.TextField;
+
+public class DialogController   {
+    public Button cancelReceipt;
+    public TextField receiptField;
+    public Button revertReceipt;
+    public Button abort;
+
+
+
+
+}

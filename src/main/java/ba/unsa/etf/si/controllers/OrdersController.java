74df6f12223e370ea112477d796764d3ac8b1732
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-04-07 22:30:15 +0200

    Controller for orders

--- src/main/java/ba/unsa/etf/si/controllers/OrdersController.java
+++ src/main/java/ba/unsa/etf/si/controllers/OrdersController.java
@@ -4,22 +4,30 @@ import ba.unsa.etf.si.App;
 import ba.unsa.etf.si.models.Order;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
+import javafx.util.Callback;
 import org.controlsfx.control.GridCell;
 import org.controlsfx.control.GridView;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class OrdersController {
 
     @FXML
-    private GridView grid;
+    private GridView<Order> grid;
 
     @FXML
     public void initialize() {
-
+        grid.setCellFactory(new OrderCellFactory());
+        grid.getItems().addAll(new Order(1L, "Denis", LocalDateTime.now()), new Order(2L, "Selimovic", LocalDateTime.now()));
     }
 
     public static class OrderCell extends GridCell<Order> {
+
+        @FXML private Label orderID, bartenderName, date;
 
         public OrderCell() {
             loadFXML();
@@ -34,6 +42,28 @@ public class OrdersController {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+
+        @Override
+        protected void updateItem(Order order, boolean empty) {
+            super.updateItem(order, empty);
+            if (empty) {
+                setText(null);
+                setContentDisplay(ContentDisplay.TEXT_ONLY);
+            } else {
+                orderID.setText(Long.toString(order.getId()));
+                orderID.setText(order.getBartender());
+                date.setText(order.getCreationDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")));
+                setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+            }
+        }
+    }
+
+    public static class OrderCellFactory implements Callback<GridView<Order>, GridCell<Order>> {
+
+        @Override
+        public GridCell<Order> call(GridView<Order> orderGridView) {
+            return new OrderCell();
         }
     }
 }

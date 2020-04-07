d58f75a739c7768936363d03f0d126a30d7d2d77
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-04-07 16:42:50 +0200

    Implemented logout

--- src/main/java/ba/unsa/etf/si/controllers/LockController.java
+++ src/main/java/ba/unsa/etf/si/controllers/LockController.java
@@ -1,5 +1,45 @@
 package ba.unsa.etf.si.controllers;
 
+import ba.unsa.etf.si.App;
+import com.jfoenix.controls.JFXButton;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+
+import static ba.unsa.etf.si.App.primaryStage;
+
 public class LockController {
 
+    @FXML
+    private Label usernameLabel;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private JFXButton loginBtn, logoutBtn;;
+
+    @FXML
+    public void initialize() {
+        usernameLabel.textProperty().bind(new SimpleStringProperty(PrimaryController.currentUser.getUsername()));
+        loginBtn.setOnAction(e -> login());
+        logoutBtn.setOnAction(e -> logout());
+    }
+
+    private void login() {
+
+    }
+
+    private void logout() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("fxml/loginForm.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            App.centerStage(primaryStage,800, 600);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }

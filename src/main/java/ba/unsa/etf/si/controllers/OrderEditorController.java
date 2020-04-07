1a3b86e38dfb1469a8edc92f27e813f0cda18585
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-04-08 01:47:14 +0200

    Added layout for adding products to order

--- src/main/java/ba/unsa/etf/si/controllers/OrderEditorController.java
+++ src/main/java/ba/unsa/etf/si/controllers/OrderEditorController.java
@@ -1,9 +1,15 @@
 package ba.unsa.etf.si.controllers;
 
 import ba.unsa.etf.si.models.Order;
+import ba.unsa.etf.si.models.Product;
 import javafx.fxml.FXML;
+import org.controlsfx.control.GridCell;
+import org.controlsfx.control.GridView;
 
 public class OrderEditorController {
+
+    @FXML
+    private GridView<Product> productsGrid;
 
     private Order order;
 
@@ -13,6 +19,11 @@ public class OrderEditorController {
 
     @FXML
     public void initialize() {
+
+    }
+
+    public class ProductGridCell extends GridCell<Product> {
+
 
     }
 }

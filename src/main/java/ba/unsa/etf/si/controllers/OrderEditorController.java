201b897fbde1cf4eb306375fdcafaf2fa5b08445
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-04-08 03:07:31 +0200

    Added factories for table view

--- src/main/java/ba/unsa/etf/si/controllers/OrderEditorController.java
+++ src/main/java/ba/unsa/etf/si/controllers/OrderEditorController.java
@@ -5,16 +5,15 @@ import ba.unsa.etf.si.models.Order;
 import ba.unsa.etf.si.models.Product;
 import ba.unsa.etf.si.utility.HttpUtils;
 import ba.unsa.etf.si.utility.IKonverzija;
-import com.jfoenix.controls.JFXButton;
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.util.Callback;
 import org.controlsfx.control.GridCell;
 import org.controlsfx.control.GridView;
@@ -47,6 +46,22 @@ public class OrderEditorController {
 
     @FXML
     public void initialize() {
+        itemName.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getName()));
+        itemQuantity.setCellFactory(new EditingCellFactory());
+        itemQuantity.setCellValueFactory(cellData -> new SimpleStringProperty(Integer.toString(cellData.getValue().getTotal())));
+        itemTotalPrice.setCellFactory(param -> new TableCell<>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                if (!empty) {
+                    int current = indexProperty().getValue();
+                    Product p = param.getTableView().getItems().get(current);
+                    setText(String.format("%.2f", p.getTotalPrice()));
+                } else {
+                    setText(null);
+                }
+            }
+        });
+
         productsGrid.setCellFactory(new ProductGridCellFactory());
         productsGrid.setVerticalCellSpacing(10);
         productsGrid.setHorizontalCellSpacing(10);
@@ -125,6 +140,97 @@ public class OrderEditorController {
         @Override
         public GridCell<Product> call(GridView<Product> productGridView) {
             return new ProductGridCell();
+        }
+    }
+
+    class EditingCell extends TableCell<Product, String> {
+
+        private TextField textField;
+
+        private EditingCell() {
+        }
+
+        @Override
+        public void startEdit() {
+            if (!isEmpty()) {
+                super.startEdit();
+                createTextField();
+                setText(null);
+                setGraphic(textField);
+            }
+        }
+
+        @Override
+        public void cancelEdit() {
+            super.cancelEdit();
+
+            setText((String) getItem());
+            setGraphic(null);
+        }
+
+        @Override
+        public void updateItem(String item, boolean empty) {
+            super.updateItem(item, empty);
+
+            if (empty) {
+                setText(item);
+                setGraphic(null);
+            } else {
+                if (isEditing()) {
+                    if (textField != null) {
+                        textField.setText(getString());
+                    }
+                    setText(null);
+                    setGraphic(textField);
+                } else {
+                    setText(getString());
+                    setGraphic(null);
+                }
+            }
+        }
+
+        private void createTextField() {
+            textField = new TextField(getString());
+            textField.setOnAction((e) -> commitEdit(textField.getText()));
+            textField.textProperty().addListener((observableValue, oldValue, newValue) -> {
+                if (!newValue.matches("[0-9\u0008]*")) {
+                    textField.setText(newValue.replaceAll("[^\\d\b]", ""));
+                }
+            });
+            textField.setOnKeyPressed(e -> {
+                if(e.getCode().equals(KeyCode.ENTER)) {
+                    int current = indexProperty().get();
+                    if(getText().isEmpty()) {
+                        getTableView().getItems().get(current).setTotal(1);
+                        setText("1");
+                    }
+                    if(getText().equals("0")) {
+                        //removeFromReceipt(current);
+                        return;
+                    }
+                    Product p = getTableView().getItems().get(current);
+                    if(p.getQuantity() < Integer.parseInt(getText())) {
+                        p.setTotal((int)p.getQuantity().doubleValue()) ;
+                        setText(Integer.toString(p.getTotal()));
+                    }
+                    else p.setTotal(Integer.parseInt(getText()));
+                    getTableView().getColumns().get(current).setVisible(false);
+                    getTableView().getColumns().get(current).setVisible(true);
+                    //price.setText(showPrice());
+                }
+            });
+        }
+
+        private String getString() {
+            return getItem() == null ? "" : getItem();
+        }
+    }
+
+    public class EditingCellFactory implements Callback<TableColumn<Product, String>, TableCell<Product, String>> {
+
+        @Override
+        public TableCell<Product, String> call(TableColumn<Product, String> productStringTableColumn) {
+            return new EditingCell();
         }
     }
 }

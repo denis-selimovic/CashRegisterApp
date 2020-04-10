e781dac79f853c785a7309d384c02623a98b9361
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-04-10 20:51:26 +0200

    implemented login dialog contoller

--- src/main/java/ba/unsa/etf/si/controllers/LoginDialogController.java
+++ src/main/java/ba/unsa/etf/si/controllers/LoginDialogController.java
@@ -1,10 +1,20 @@
 package ba.unsa.etf.si.controllers;
 
+import ba.unsa.etf.si.App;
+import ba.unsa.etf.si.utility.HttpUtils;
 import ba.unsa.etf.si.utility.interfaces.TokenReceiver;
 import com.jfoenix.controls.JFXButton;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+import org.json.JSONObject;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class LoginDialogController {
 
@@ -29,6 +39,30 @@ public class LoginDialogController {
     private void initialize() {
         title.setText(dialogTitle);
         header.setText(dialogHeader);
+        password.focusedProperty().addListener((observableValue, aBoolean, t1) -> {
+            password.getStyleClass().removeAll("wrong-pw");
+        });
+        login.setOnAction(e -> login());
+    }
 
+    private void login() {
+        HttpRequest.BodyPublisher bodyPublisher = HttpRequest.BodyPublishers
+                .ofString("{\"username\": \"" + PrimaryController.currentUser.getUsername() + "\","
+                        + "\"password\": \"" + password.getText() + "\"}");
+        HttpRequest POST = HttpUtils.POST(bodyPublisher, App.DOMAIN + "/api/login", "Content-Type", "application/json");
+        HttpUtils.send(POST, HttpResponse.BodyHandlers.ofString(), response -> {
+            JSONObject objResponse = new JSONObject(response);
+            if(!objResponse.isNull("error")) wrongPassword();
+            else closeDialog(objResponse.getString("token"));
+        }, this::wrongPassword);
+    }
+
+    private void closeDialog(String token) {
+        receiver.onTokenReceived(token);
+        ((Stage) header.getScene().getWindow()).close();
+    }
+
+    private void wrongPassword() {
+        password.getStyleClass().add("wrong-pw");
     }
 }

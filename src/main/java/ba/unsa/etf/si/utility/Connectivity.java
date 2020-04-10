95fe2d5b8805bebc71717d642d34313ccb102130
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-04-10 15:36:15 +0200

    Changes in utility class

--- src/main/java/ba/unsa/etf/si/utility/Connectivity.java
+++ src/main/java/ba/unsa/etf/si/utility/Connectivity.java
@@ -18,7 +18,7 @@ public class Connectivity {
 
     private final String target;
 
-    private static final int INTERVAL = 15; //repeat after 30s
+    private static final int INTERVAL = 15; //repeat after 15s
     private static final int PORT = 80;
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
@@ -59,6 +59,7 @@ public class Connectivity {
 
     public void run() {
         scheduler.scheduleWithFixedDelay(() -> {
+            removeNulls();
             if (isReachable()) {
                 onlineMode();
                 System.out.println("ONLINE!");
@@ -67,7 +68,6 @@ public class Connectivity {
                 offlineMode();
                 System.out.println("OFFLINE!");
             }
-            removeNulls();
         }, 0, INTERVAL, TimeUnit.SECONDS);
     }
 }

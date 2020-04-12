8896a581538a1438887d966d74382c93081561a8
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-04-12 14:12:13 +0200

    Changed imports

--- src/main/java/ba/unsa/etf/si/utility/PDFCashierBalancingFactory.java
+++ src/main/java/ba/unsa/etf/si/utility/PDFCashierBalancingFactory.java
@@ -23,7 +23,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;

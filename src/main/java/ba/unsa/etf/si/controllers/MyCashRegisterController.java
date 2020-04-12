b7e6dea8c2376011956ea7515de4917fa6953ea7
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-04-12 20:52:41 +0200

    final update

--- src/main/java/ba/unsa/etf/si/controllers/MyCashRegisterController.java
+++ src/main/java/ba/unsa/etf/si/controllers/MyCashRegisterController.java
@@ -97,7 +97,6 @@ public class MyCashRegisterController implements PaymentProcessingListener, Conn
 
     public MyCashRegisterController(Receipt receipt) {
         revertedReceipt = receipt;
-        System.out.println(receipt.getServerID());
         if(receipt.getServerID() != null) sellerReceiptID = receipt.getServerID();
         else sellerReceiptID = -1;
         App.connectivity.subscribe(this);
@@ -349,7 +348,6 @@ public class MyCashRegisterController implements PaymentProcessingListener, Conn
         Receipt receipt = new Receipt(LocalDateTime.now(), PrimaryController.currentUser.getUsername(), price());
         for(Product p : receiptTable.getItems()) receipt.getReceiptItems().add(new ReceiptItem(p));
         if(sellerReceiptID != -1) receipt.setServerID(sellerReceiptID);
-        System.out.println(sellerReceiptID);
         return receipt;
     }
 

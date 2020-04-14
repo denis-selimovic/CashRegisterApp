14297e31bbc8a2dc184512ea485a6badc427d2ff
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-04-14 23:21:17 +0200

    changes

--- src/main/java/ba/unsa/etf/si/utility/date/DateUtils.java
+++ src/main/java/ba/unsa/etf/si/utility/date/DateUtils.java
@@ -1,10 +1,12 @@
 package ba.unsa.etf.si.utility.date;
 
 import ba.unsa.etf.si.models.Receipt;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,4 +22,19 @@ public class DateUtils {
         return receipts.stream().filter(r -> compareDates(date, LocalDate.from(r.getDate()))).collect(Collectors.toList());
     }
 
+    public static Date asDate(LocalDate localDate) {
+        return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static Date asDate(LocalDateTime localDateTime) {
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static LocalDate asLocalDate(Date date) {
+        return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+
+    public static LocalDateTime asLocalDateTime(Date date) {
+        return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
 }

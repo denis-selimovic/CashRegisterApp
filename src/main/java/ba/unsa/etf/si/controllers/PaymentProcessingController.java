9d7d550a57e593c76b0798178398812e49125d24
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-04-15 13:33:26 +0200

    added utils for qr string

--- src/main/java/ba/unsa/etf/si/controllers/PaymentProcessingController.java
+++ src/main/java/ba/unsa/etf/si/controllers/PaymentProcessingController.java
@@ -1,11 +1,10 @@
 package ba.unsa.etf.si.controllers;
 
-import ba.unsa.etf.si.App;
 import ba.unsa.etf.si.models.Receipt;
-import ba.unsa.etf.si.models.ReceiptItem;
 import ba.unsa.etf.si.models.enums.PaymentMethod;
-import ba.unsa.etf.si.utility.payment.CreditCardServer;
 import ba.unsa.etf.si.utility.image.QRUtils;
+import ba.unsa.etf.si.utility.json.QRJsonUtils;
+import ba.unsa.etf.si.utility.payment.CreditCardServer;
 import ba.unsa.etf.si.utility.payment.CreditInfoReceiver;
 import com.jfoenix.controls.JFXProgressBar;
 import javafx.application.Platform;
@@ -13,8 +12,6 @@ import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import org.apache.commons.lang3.StringUtils;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class PaymentProcessingController {
@@ -29,46 +26,17 @@ public class PaymentProcessingController {
     private PaymentMethod paymentMethod;
 
     @FXML
-    public void initialize() {
-
-    }
+    public void initialize() { }
 
     public void setQRTypeAndCode(Receipt receipt, boolean isDynamic) {
-        if (isDynamic) {
-
-            StringBuilder receiptItems = new StringBuilder();
-            List<ReceiptItem> receiptItemArrayList = receipt.getReceiptItems();
-            for (ReceiptItem receiptItem : receiptItemArrayList) {
-                int itemQuantity = (int) receiptItem.getQuantity();
-                receiptItems.append(receiptItem.getName()).append(" (").append(itemQuantity).append(")");
-                receiptItems.append(",");
-            }
-            String receiptItemsString = StringUtils.chop(receiptItems.toString());
-
-            qrCodeString = "{\n" +
-                    "\"cashRegisterId\": " + App.getCashRegisterID() + ",\n" +
-                    "\"officeId\": " + App.getBranchID() + ",\n" +
-                    "\"businessName\": \"BINGO\",\n" +
-                    "\"receiptId\": \"" + receipt.getTimestampID() + "\",\n" +
-                    "\"service\": \"" + receiptItemsString + "\",\n" +
-                    "\"totalPrice\": " + receipt.getAmount() + "\n" +
-                    "}";
-        } else {
-            qrCodeString = "{\n" +
-                    "\"cashRegisterId\": " + App.getCashRegisterID() + ",\n" +
-                    "\"officeId\": " + App.getBranchID() + ",\n" +
-                    "\"businessName\": \"BINGO\"\n" +
-                    "}";
-        }
+        qrCodeString = (isDynamic) ? QRJsonUtils.getDynamicQRCode(receipt) : QRJsonUtils.getStaticQRCode();
     }
 
     public void processPayment(PaymentMethod paymentMethod, PaymentController paymentController, double totalAmount) {
         this.paymentMethod = paymentMethod;
         this.paymentController = paymentController;
-        if (paymentMethod == PaymentMethod.CASH || paymentMethod == PaymentMethod.PAY_APP)
-            fillProgressBar(true, "");
-        else
-            fetchCreditCardInfo(totalAmount);
+        if (paymentMethod == PaymentMethod.CASH || paymentMethod == PaymentMethod.PAY_APP) fillProgressBar(true, "");
+        else fetchCreditCardInfo(totalAmount);
     }
 
     public void fetchCreditCardInfo(Double totalAmount) {
@@ -79,61 +47,55 @@ public class PaymentProcessingController {
 
     public void fillProgressBar(boolean isValid, String creditCardInfo) {
         new Thread(() -> {
-            try {
-                for (int i = 0; i <= 100; i++) {
-                    double progress = i * 0.01;
-                    Platform.runLater(() -> paymentProgress.setProgress(progress));
-                    Thread.sleep(15);
-                }
+            loading();
 
-                if (paymentMethod == PaymentMethod.CASH) {
+            if (paymentMethod == PaymentMethod.CASH) {
+                CompletableFuture.runAsync(() -> paymentController.saveReceipt())
+                        .handle((obj, ex) -> {
+                            showMessage(ex == null);
+                            return null;
+                        });
+            }
+            if (paymentMethod == PaymentMethod.PAY_APP) {
+                CompletableFuture.runAsync(() -> paymentController.saveReceipt())
+                        .thenRunAsync(() -> Platform.runLater(() -> {
+                                    try {
+                                        paymentProgress.setVisible(false);
+                                        qrCode.setVisible(true);
+                                        qrCode.setImage(QRUtils.getQRImage(qrCodeString, 300, 300));
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                })
+
+                        ).thenRunAsync(() -> sleep(100000)).thenRunAsync(() -> paymentController.pollForResponse())
+                        .handle((obj, ex) -> {
+                            showMessage(ex == null);
+                            return null;
+                        });
+            }
+            if (paymentMethod == PaymentMethod.CREDIT_CARD) {
+                if (!isValid) {
+                    infoText.setText(creditCardInfo);
+                    paymentProgress.setVisible(false);
+                    showMessage(false);
+                } else {
                     CompletableFuture.runAsync(() -> paymentController.saveReceipt())
                             .handle((obj, ex) -> {
                                 showMessage(ex == null);
                                 return null;
                             });
                 }
-                if (paymentMethod == PaymentMethod.PAY_APP) {
-                    CompletableFuture.runAsync(() -> paymentController.saveReceipt())
-                            .thenRunAsync(() -> Platform.runLater(() -> {
-                                        try {
-                                            paymentProgress.setVisible(false);
-                                            qrCode.setVisible(true);
-                                            qrCode.setImage(QRUtils.getQRImage(qrCodeString, 300, 300));
-                                        } catch (Exception e) {
-                                            e.printStackTrace();
-                                        }
-                                    })
-
-                            ).thenRunAsync(() -> {
-                        try {
-                            Thread.sleep(10000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }).thenRunAsync(() -> paymentController.pollForResponse())
-                            .handle((obj, ex) -> {
-                                showMessage(ex == null);
-                                return null;
-                            });
-                }
-                if (paymentMethod == PaymentMethod.CREDIT_CARD) {
-                    if (!isValid) {
-                        infoText.setText(creditCardInfo);
-                        paymentProgress.setVisible(false);
-                        showMessage(false);
-                    } else {
-                        CompletableFuture.runAsync(() -> paymentController.saveReceipt())
-                                .handle((obj, ex) -> {
-                                    showMessage(ex == null);
-                                    return null;
-                                });
-                    }
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
         }).start();
+    }
+
+    private void loading() {
+        for (int i = 0; i <= 100; i++) {
+            double progress = i * 0.01;
+            Platform.runLater(() -> paymentProgress.setProgress(progress));
+            sleep(15);
+        }
     }
 
     private void showMessage(boolean valid) {
@@ -147,5 +109,13 @@ public class PaymentProcessingController {
         }
         Platform.runLater(() -> ((Stage) statusText.getScene().getWindow()).close());
         paymentController.onPaymentProcessed(valid);
+    }
+
+    private void sleep(long milis) {
+        try {
+            Thread.sleep(milis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }

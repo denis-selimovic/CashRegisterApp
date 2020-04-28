b5eccb77161e8195b31919a7b19aea957ad150c1
dselimovic <dselimovic1@etf.unsa.ba>
2020-04-28 14:18:47 +0200

    session handler added

---
+++ src/main/java/ba/unsa/etf/si/utility/sockets/NotificationStompSessionHandler.java
@@ -0,0 +1,36 @@
+package ba.unsa.etf.si.utility.sockets;
+
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+
+import java.lang.reflect.Type;
+
+public class NotificationStompSessionHandler implements StompSessionHandler {
+
+    @Override
+    public void afterConnected(StompSession stompSession, StompHeaders stompHeaders) {
+
+    }
+
+    @Override
+    public void handleException(StompSession stompSession, StompCommand stompCommand, StompHeaders stompHeaders, byte[] bytes, Throwable throwable) {
+
+    }
+
+    @Override
+    public void handleTransportError(StompSession stompSession, Throwable throwable) {
+
+    }
+
+    @Override
+    public Type getPayloadType(StompHeaders stompHeaders) {
+        return null;
+    }
+
+    @Override
+    public void handleFrame(StompHeaders stompHeaders, Object o) {
+
+    }
+}

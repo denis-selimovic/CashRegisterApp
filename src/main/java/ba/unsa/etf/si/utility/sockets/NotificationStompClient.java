5f25d58ac346cffe91efc1f208c8816eefa7d86e
dselimovic <dselimovic1@etf.unsa.ba>
2020-04-28 14:50:24 +0200

    Added sock js utils

--- src/main/java/ba/unsa/etf/si/utility/sockets/NotificationStompClient.java
+++ src/main/java/ba/unsa/etf/si/utility/sockets/NotificationStompClient.java
@@ -3,15 +3,7 @@ package ba.unsa.etf.si.utility.sockets;
 import ba.unsa.etf.si.utility.interfaces.SessionInitializer;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.stomp.StompSession;
-import org.springframework.web.socket.client.WebSocketClient;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
-import org.springframework.web.socket.sockjs.client.SockJsClient;
-import org.springframework.web.socket.sockjs.client.Transport;
-import org.springframework.web.socket.sockjs.client.WebSocketTransport;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class NotificationStompClient implements SessionInitializer {
 
@@ -22,24 +14,13 @@ public class NotificationStompClient implements SessionInitializer {
     private StompSession stompSession;
 
     public NotificationStompClient() {
-        stompClient = new WebSocketStompClient(getSockJsClient());
+        stompClient = new WebSocketStompClient(SockJSUtils.getSockJsClient());
         initialize();
     }
 
     private void initialize() {
         stompClient.setMessageConverter(new MappingJackson2MessageConverter());
         stompClient.connect(URL, new NotificationStompSessionHandler(this));
-    }
-
-    private static SockJsClient getSockJsClient() {
-        return new SockJsClient(addTransports());
-    }
-
-    private static List<Transport> addTransports() {
-        WebSocketClient simpleWebSocketClient = new StandardWebSocketClient();
-        List<Transport> transports = new ArrayList<>(1);
-        transports.add(new WebSocketTransport(simpleWebSocketClient));
-        return transports;
     }
 
     @Override

8adb7c2793e02afcb4e68e0f89677130c3fe705a
dselimovic <dselimovic1@etf.unsa.ba>
2020-05-03 22:20:01 +0200

    app changes

--- src/main/java/ba/unsa/etf/si/App.java
+++ src/main/java/ba/unsa/etf/si/App.java
@@ -25,16 +25,13 @@ public class App extends Application {
     public static final Long MERCHANT_ID = 1L;
     public static String UUID = "";
     public static final String TARGET = DOMAIN + "/api/test";
-    public static String UUID = null;
 
     public static Long getCashRegisterID() {
         return CASH_REGISTER_ID;
     }
-
     public static Long getBranchID() {
         return BRANCH_ID;
     }
-
     public static Long getMerchantID() {
         return MERCHANT_ID;
     }
@@ -42,9 +39,6 @@ public class App extends Application {
     public static final Connectivity connectivity = new Connectivity(TARGET);
     public static final NotificationStompClient stompClient = new NotificationStompClient();
 
-    public static void setUUID (String uuid) {
-        UUID = uuid;
-    }
     @Override
     public void start(Stage stage) throws IOException {
         primaryStage = stage;

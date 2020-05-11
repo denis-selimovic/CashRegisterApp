167930d9952bc72f6e61279ae71aa066fc7316d6
dselimovic <dselimovic1@etf.unsa.ba>
2020-05-11 02:47:19 +0200

    added repository

---
+++ src/main/java/ba/unsa/etf/si/persistance/repository/CashRegisterRepository.java
@@ -0,0 +1,54 @@
+package ba.unsa.etf.si.persistance.repository;
+
+import ba.unsa.etf.si.models.CashRegister;
+import ba.unsa.etf.si.models.Credentials;
+import ba.unsa.etf.si.persistance.utility.HibernateFactory;
+import ba.unsa.etf.si.persistance.utility.Repository;
+import org.hibernate.Session;
+
+import java.util.List;
+
+public class CashRegisterRepository implements Repository<CashRegister> {
+    @Override
+    public void update(CashRegister item) {
+        try (Session session = HibernateFactory.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.saveOrUpdate(item);
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public void delete(CashRegister item) {
+        try (Session session = HibernateFactory.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.delete(item);
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public void add(CashRegister item) {
+        try (Session session = HibernateFactory.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.persist(item);
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public CashRegister get(Long id) {
+        CashRegister cashRegister = null;
+        try (Session session = HibernateFactory.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            cashRegister = session.get(CashRegister.class, id);
+            session.getTransaction().commit();
+        }
+        return cashRegister;
+    }
+
+    @Override
+    public List<CashRegister> getAll() {
+        return null;
+    }
+}

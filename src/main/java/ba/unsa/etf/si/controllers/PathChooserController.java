7a34a94615dae95a296dfbc399db9f94f292cec2
dselimovic <dselimovic1@etf.unsa.ba>
2020-05-15 01:41:41 +0200

    made fxml for controller

---
+++ src/main/java/ba/unsa/etf/si/controllers/PathChooserController.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.si.controllers;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+
+public class PathChooserController {
+
+    @FXML
+    private JFXButton submitBtn;
+    @FXML
+    private PasswordField pwField;
+
+
+}
